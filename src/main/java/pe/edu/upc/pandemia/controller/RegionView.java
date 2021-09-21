7285675b9d9f3502e9643b21ee283cccf2c84136
Juan Flores Moroc <pcsijflo@upc.edu.pe>
2021-09-21 12:34:25 -0500

    Update Datatable

--- src/main/java/pe/edu/upc/pandemia/controller/RegionView.java
+++ src/main/java/pe/edu/upc/pandemia/controller/RegionView.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.pandemia.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -18,17 +19,27 @@ public class RegionView implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private List<Region> regions;
+	private Region regionSelected;
+	private List<Region> regionsSelected;	
 	
 	@Inject
 	private RegionService regionService;
 	
 	@PostConstruct
 	public void init() {
+		regionsSelected = new ArrayList<>();
 		try {
 			regions = regionService.getAll();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public boolean hasRegionsSelected() {
+		if (regionsSelected.isEmpty()) {
+			return false;
+		}
+		return true;
 	}
 	
 	public List<Region> getRegions() {
@@ -39,4 +50,20 @@ public class RegionView implements Serializable {
 		this.regionService = regionService;
 	}
 
+	public Region getRegionSelected() {
+		return regionSelected;
+	}
+
+	public void setRegionSelected(Region regionSelected) {
+		this.regionSelected = regionSelected;
+	}
+
+	public List<Region> getRegionsSelected() {
+		return regionsSelected;
+	}
+
+	public void setRegionsSelected(List<Region> regionsSelected) {
+		this.regionsSelected = regionsSelected;
+	}
+	
 }
